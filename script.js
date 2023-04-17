d9d5d24ce796de6212101800ec309b1ab23ff989
Cassandra Cawley <cassandrabook09@gmail.com>
2023-04-17 12:38:32 -0700

    did addBookToLibrary

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ Book.prototype.getInfo = function() {
     console.log(`${this.Title} by ${this.Author}, ${this.Pages}, ${this.Read}`)
 }
 
+//const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 200, "read");
+//const thud = new Book("Thud!", "Terry Pratchett", 439, "read");
+//const revenant = new Book("Revenant Gun", "Yoon Ha Lee", 427, "not read");
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title,author,pages,read) {
+    let newBook = new Book(title,author,pages,read);
+    myLibrary.push(newBook);
 }
