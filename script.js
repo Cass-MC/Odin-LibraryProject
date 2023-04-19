ad9cae0a61d82fc5f2a66f2b7cc6afc872fab9fa
Cassandra Cawley <cassandrabook09@gmail.com>
2023-04-19 13:07:14 -0700

    added displayBooksOnShelf

--- script.js
+++ script.js
@@ -8,14 +8,26 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.getInfo = function() {
-    console.log(`${this.Title} by ${this.Author}, ${this.Pages}, ${this.Read}`)
+    return `${this.Title} by ${this.Author}, ${this.Pages}, ${this.Read}`;
 }
 
-//const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 200, "read");
-//const thud = new Book("Thud!", "Terry Pratchett", 439, "read");
-//const revenant = new Book("Revenant Gun", "Yoon Ha Lee", 427, "not read");
+const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 200, "read");
+const thud = new Book("Thud!", "Terry Pratchett", 439, "read");
+const revenant = new Book("Revenant Gun", "Yoon Ha Lee", 427, "not read");
+
+myLibrary.push(hobbit,thud,revenant);
 
 function addBookToLibrary(title,author,pages,read) {
     let newBook = new Book(title,author,pages,read);
     myLibrary.push(newBook);
+}
+
+function displayBooksOnShelf() {
+    for (const book of myLibrary) {
+        const bookNode = document.createElement("div");
+        bookNode.className = "bookCard";
+        bookNode.innerText = book.getInfo();
+        document.getElementById("libraryShelf").appendChild(bookNode);
+        console.log(bookNode);
+    }
 }
