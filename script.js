9435bb179929fe41961c4962f643938f6e924f6c
Cassandra Cawley <cassandrabook09@gmail.com>
2023-04-21 09:39:36 -0700

    started toggleButtonClick, has errors rn

--- script.js
+++ script.js
@@ -11,9 +11,14 @@ Book.prototype.getInfo = function() {
     return `${this.Title} by ${this.Author}, ${this.Pages}, ${this.Read}`;
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 200, "read");
-const thud = new Book("Thud!", "Terry Pratchett", 439, "read");
-const revenant = new Book("Revenant Gun", "Yoon Ha Lee", 427, "not read");
+Book.prototype.toggleRead = function() {
+    if (this.Read == "Yes") { this.Read = "No"; }
+    if (this.Read == "No") { this.Read = "Yes"; }
+}
+
+const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 200, "Yes");
+const thud = new Book("Thud!", "Terry Pratchett", 439, "Yes");
+const revenant = new Book("Revenant Gun", "Yoon Ha Lee", 427, "No");
 
 myLibrary.push(hobbit,thud,revenant);
 
@@ -42,13 +47,46 @@ function displayBooksOnShelf() {
         const bookNode = document.createElement("div");
         bookNode.className = "bookCard";
         bookNode.innerText = book.getInfo();
-        bookNode.dataset.index = myLibrary.indexOf(book);
+        
         const bookRemove = document.createElement("button");
         bookRemove.innerText = "Remove";
         bookRemove.className = "bookRemoveButton";
+        bookRemove.dataset.index = myLibrary.indexOf(book);
         bookNode.appendChild(bookRemove);
+
+        let toggleHtml = "<input type=\"checkbox\" id=\"toggle\"";
+        if (book.Read == "Yes") {
+            toggleHtml += " checked";
+        }
+        toggleHtml += "/><label for=\"toggle\">Read</label>";
+        const bookToggle = document.createElement("div");
+        bookToggle.className = "bookToggleButton";
+        bookToggle.innerHTML = toggleHtml;
+        bookToggle.dataset.index = myLibrary.indexOf(book);
+        bookNode.appendChild(bookToggle);
+
         libraryShelfNode.appendChild(bookNode);
     }
+
+    for (const removeButton of document.querySelectorAll(".bookRemoveButton")) {
+        removeButton.addEventListener("click", () => { removeBookFromShelf(removeButton); });
+    }
+
+    for (const toggleButton of document.querySelectorAll(".bookToggleButton")) {
+        toggleButton.addEventListener("click", () => { toggleButtonClick(toggleButton); });
+    }
+}
+
+function removeBookFromShelf(removeButton) {
+    const bookIndex = removeButton.dataset.index;
+    myLibrary.splice(bookIndex, 1);
+    displayBooksOnShelf();
+}
+
+function toggleButtonClick(toggleButton) {
+    const toggleIndex = toggleButton.dataset.index;
+    myLibrary[toggleIndex].toggleRead();
+    //Not toggling correctly- still returns as Yes
 }
 
 function formPopUp() {
